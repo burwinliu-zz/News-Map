055adee8f9e914b193d1281219a4f04d8cbc5ae6
burwinliu <burwincollege@gmail.com>
2019-07-20 15:41:03 -0700

    Working version of webpage -- can now display map with npm start when in the webpage directory. Remember to install all dependencies (ol only as of right now I think)and have npm set up.

--- webpage/scripts/index.js
+++ webpage/scripts/index.js
@@ -3,15 +3,15 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
-var map = new ol.Map({
-    target: 'map',9
+const map = new Map({
+    target: 'map',
     layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
+        new TileLayer({
+            source: new OSM()
+        })
     ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([37.41, 8.82]),
-      zoom: 4
+    view: new View({
+    center: [0, 0],
+    zoom: 0
     })
 });

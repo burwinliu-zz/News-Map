bce1b77495f681c94fbc60a226086722c0e8939f
burwinliu <burwincollege@gmail.com>
2019-08-01 12:53:28 -0700

    updated json and trying to update the js script

--- webpage/scripts/index.js
+++ webpage/scripts/index.js
@@ -1,62 +1,115 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
-import 'ol/proj'
+import {fromLonLat} from 'ol/proj'
 import {GeoJSON} from "ol/format";
-import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import VectorLayer from 'ol/layer/Vector';
-import {Style, Fill, Stroke} from "ol/style";
+import {Tile} from "ol/layer";
+import {Style, Fill, Stroke, Circle} from "ol/style";
 import VectorSource from "ol/source/Vector";
 
 
 /*
+    TODO: Find a way to access these data-points from local post a npm start (using parcel)
+
     Data for borders.json retrieved from http://thematicmapping.org/downloads/world_borders.php
+
+    Data for countries.geojson from https://github.com/datasets/geo-countries
+        All data is licensed under the Open Data Commons Public Domain Dedication and License.
+        Note that the original data from Natural Earth is public domain. While no credit is formally required a link
+        back or credit to Natural Earth, Lexman and the Open Knowledge Foundation is much appreciated.
+        All source code is licenced under the MIT licence.
+
+    Data for custom.json is from https://github.com/AshKyd/geojson-regions under The Un-license
+*/
+
+/*
+var map = new Map({
+    target: 'map',
+    layers: [
+      new Tile({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+});
+*/
+
+/*
+const countryStyle = new Style({
+    fill: new Fill({
+      color: [203, 194, 185, 1]
+    }),
+    stroke: new Stroke({
+      color: [177, 163, 148, 0.5],
+      width: 2,
+      lineCap: 'round'
+    })
+});
+
+const countries = new Vector({
+    source: new GeoJSON({
+      projection: 'EPSG:3857',
+      url: './data/borders.json',
+    }),
+    style: countryStyle
+});
 */
 
 
-const _myStroke = new Stroke({
-   color : '#FFFF00',
-   width : 1
+const tile =  new Tile({
+    source: new OSM()
 });
 
-const _myFill = new Fill({
-   color: '#FFFF00'
+const mapView = new View({
+    center: fromLonLat([37.41, 8.82]),
+    zoom: 4
 });
 
-const myStyle = new Style({
-   stroke : _myStroke,
-   fill : _myFill
- });
-
-
-new Map({
+const url_loc = 'C:\\Users\\terra\\PycharmProjects\\News-Map\\webpage\\scripts\\data\\custom.json'
+const map = new Map({
     target: 'map',
     layers: [
-        new TileLayer({
-            source: new OSM(),
-        }),
-        // New VectorLayer with VectorSource and the countries.geojson file as source
+        tile,
         new VectorLayer({
             source: new VectorSource({
-                // Trying to look for better solution to find the countries borders -- looking at
-                // http://thematicmapping.org/downloads/world_borders.php
-                url: './data/borders.json',
                 format: new GeoJSON(),
+                url: url_loc
             }),
-            style: myStyle,
-        }),
-    ],
-    view: new View({
-        center: [0, 0],
-        zoom: 2,
-    }),
-    style: new Style({
-        stroke: new Stroke({
-            color: '#f00',
-            width: 1
-        }),
-        fill: new Fill({
-            color: 'rgba(255,0,0,0.1)'
+            style: new Style({
+                fill: new Fill({
+                    color: '#D4AF37',
+                })
+            })
         })
-    })
+    ],
+    view: mapView
 });
+
+
+const countryLayer = new VectorLayer({
+    source: new VectorSource({
+        format: new GeoJSON(),
+        url: 'C:\\Users\\terra\\PycharmProjects\\News-Map\\webpage\\scripts\\data\\custom.json',
+    }),
+    projection: map.getView().getProjection(),
+    /*
+    style: function(feature) {
+        return({
+            fill: new Fill({
+                color: '#000000'
+            }),
+            stroke: new Stroke({
+                color: '#000000',
+                width: 2,
+                lineCap: 'round'
+            })
+        })
+    }
+    */
+});
+
+//map.addLayer(countryLayer);

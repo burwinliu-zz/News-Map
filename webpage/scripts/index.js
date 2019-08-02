1b1c64f43f5cfcf647d0e894d01994da72e7689a
burwinliu <burwincollege@gmail.com>
2019-08-01 22:24:46 -0700

    stable version -- can colour ISSUES: need to find way to optimize speeds with ol (Slow loading) and json loading

--- webpage/scripts/index.js
+++ webpage/scripts/index.js
@@ -7,6 +7,7 @@ import VectorLayer from 'ol/layer/Vector';
 import {Tile} from "ol/layer";
 import {Style, Fill, Stroke, Circle} from "ol/style";
 import VectorSource from "ol/source/Vector";
+import * as $ from "jquery"
 
 
 /*
@@ -23,93 +24,38 @@ import VectorSource from "ol/source/Vector";
     Data for custom.json is from https://github.com/AshKyd/geojson-regions under The Un-license
 */
 
-/*
-var map = new Map({
-    target: 'map',
-    layers: [
-      new Tile({
-        source: new OSM()
-      })
-    ],
-    view: new View({
-      center: fromLonLat([37.41, 8.82]),
-      zoom: 4
-    })
-});
-*/
-
-/*
-const countryStyle = new Style({
-    fill: new Fill({
-      color: [203, 194, 185, 1]
-    }),
-    stroke: new Stroke({
-      color: [177, 163, 148, 0.5],
-      width: 2,
-      lineCap: 'round'
-    })
-});
-
-const countries = new Vector({
-    source: new GeoJSON({
-      projection: 'EPSG:3857',
-      url: './data/borders.json',
-    }),
-    style: countryStyle
-});
-*/
-
-
-const tile =  new Tile({
-    source: new OSM()
-});
-
 const mapView = new View({
     center: fromLonLat([37.41, 8.82]),
     zoom: 4
 });
 
-const url_loc = 'C:\\Users\\terra\\PycharmProjects\\News-Map\\webpage\\scripts\\data\\custom.json'
+const countryStyle = new Style({
+    fill: new Fill({
+        color: '#D4AF37',
+    })
+});
+
+const countryColours = new VectorLayer({
+    source: new VectorSource({
+        format: new GeoJSON(),
+        //Temporary fix by using github raw data
+        // TODO: Make a new solution with local access of json file (see getJSON from jquery?)
+        url: "https://raw.githubusercontent.com/burwinliu/News-Map/master/webpage/scripts/data/countries.geojson",
+    }),
+    style: countryStyle,
+});
+
+const tile =  new Tile({
+    source: new OSM(),
+    style: countryStyle,
+});
+
 const map = new Map({
     target: 'map',
     layers: [
         tile,
-        new VectorLayer({
-            source: new VectorSource({
-                format: new GeoJSON(),
-                url: url_loc
-            }),
-            style: new Style({
-                fill: new Fill({
-                    color: '#D4AF37',
-                })
-            })
-        })
+        countryColours
     ],
-    view: mapView
+    view: mapView,
+    style: countryStyle
 });
-
-
-const countryLayer = new VectorLayer({
-    source: new VectorSource({
-        format: new GeoJSON(),
-        url: 'C:\\Users\\terra\\PycharmProjects\\News-Map\\webpage\\scripts\\data\\custom.json',
-    }),
-    projection: map.getView().getProjection(),
-    /*
-    style: function(feature) {
-        return({
-            fill: new Fill({
-                color: '#000000'
-            }),
-            stroke: new Stroke({
-                color: '#000000',
-                width: 2,
-                lineCap: 'round'
-            })
-        })
-    }
-    */
-});
-
-//map.addLayer(countryLayer);
